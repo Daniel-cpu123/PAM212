805b38fc9eafb687a72fdcedf3f6e46451ffef59
Daniel Alexandro Castelo Castillo <124051585@upq.edu.mx>
2025-09-11 16:18:11 -0600

    Probando comentarios y variables

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,6 +1,13 @@
+
+//probando comentarios
+var nameuser = 'jonh'
+let lastname = 'carter'
 console.log(lastname);
 nameuser= 'pepe';
 
 
 const PI = 3.1416
 console.log(PI)
+
+//camelcase
+let userName = 'gordon'
