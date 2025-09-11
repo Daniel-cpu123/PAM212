12b37c3fec6b28778112506fbeacee34603475db
Daniel Alexandro Castelo Castillo <124051585@upq.edu.mx>
2025-09-11 16:07:04 -0600

    Update app.js

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,25 +1,2 @@
 
-console.log("Hello World");
-
-//tipos de datos
-"HELLO WORLD" // String
-'HELLO WORLD' // string comillas simples
-100000 // num int
-10.1 // números decimales
-
-// booleanos
-true
-false
-
-// array
-['joe','ryan','marta']
-[1,2,3,4]
-[true,false,false]
-
-//object
-'ryan'
-70.7
-14
-true
-
-
+console.log("esto es un string")
