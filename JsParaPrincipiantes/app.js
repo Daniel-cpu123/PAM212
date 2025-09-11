ff70a9d7bfad5d706425f65c9d8694c65892423c
Daniel Alexandro Castelo Castillo <124051585@upq.edu.mx>
2025-09-11 16:05:12 -0600

    Tipos de datos

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,7 +1,25 @@
-function add (n1,n2){
-    console.log(n1+n2)
 
-}
+console.log("Hello World");
 
-add (3,2)
-add(100,2)
+//tipos de datos
+"HELLO WORLD" // String
+'HELLO WORLD' // string comillas simples
+100000 // num int
+10.1 // números decimales
+
+// booleanos
+true
+false
+
+// array
+['joe','ryan','marta']
+[1,2,3,4]
+[true,false,false]
+
+//object
+'ryan'
+70.7
+14
+true
+
+
