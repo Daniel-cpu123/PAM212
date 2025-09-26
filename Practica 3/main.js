04611d71bf663b4af21fe91597480d4b7222332e
Daniel Alexandro Castelo Castillo <124051585@upq.edu.mx>
2025-09-25 18:37:42 -0600

    Ejercicio A Import y Export

---
+++ Practica 3/main.js
@@ -0,0 +1,7 @@
+const restar = require('./utils');
+
+// Pruebas de la función
+console.log("Prueba 1: 10 - 5 =", restar(10, 5));
+console.log("Prueba 2: 20 - 7 =", restar(20, 7));
+console.log("Prueba 3: 3 - 8 =", restar(3, 8));
+console.log("Prueba 4: 0 - 0 =", restar(0, 0));
